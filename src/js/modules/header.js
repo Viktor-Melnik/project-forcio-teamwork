cb5ebf4910d6dce7eaca1df266570c9f4882a06c
Melnik Viktor <melnikviktor82@gmail.com>
2023-02-24 18:19:15 +0200

    day3

--- src/js/modules/header.js
+++ src/js/modules/header.js
@@ -1,4 +1,20 @@
 // логіка для секції header. Кодить Віктор
-document.querySelector(".main-header__burger").addEventListener("click", function() {
+
+const burger = document.querySelector(".main-header__burger");
+const nav = document.querySelector(".main-header__nav");
+
+
+burger.addEventListener("click", function() {
     this.classList.toggle("main-header__burger--active");
+
+    nav.classList.toggle("main-header__nav--active");
 })
+
+window.onresize = viewport;
+
+function viewport() {
+    if (document.documentElement.clientWidth > 480) {
+        burger.classList.remove("main-header__burger--active");
+        nav.classList.remove("main-header__nav--active");
+    }
+}
