29137c110d53fbefcecc3b838a10e106068464c6
Melnik Viktor <melnikviktor82@gmail.com>
2023-02-25 12:26:25 +0200

    total fix

--- src/js/modules/header.js
+++ src/js/modules/header.js
@@ -4,11 +4,20 @@ const burger = document.querySelector(".main-header__burger");
 const nav = document.querySelector(".main-header__nav");
 
 
-burger.addEventListener("click", function() {
-    this.classList.toggle("main-header__burger--active");
-
+burger.addEventListener("click", function(event) {
+    burger.classList.toggle("main-header__burger--active");
     nav.classList.toggle("main-header__nav--active");
+
+    event.isClick = true;
 })
+
+document.body.addEventListener("click", function(event) {
+    if (event.isClick === true || event.target.classList.contains("main-header__nav-link") 
+    || event.target.classList.contains("main-header__nav-list-item") || event.target.classList.contains("main-header__nav")) return
+    burger.classList.remove("main-header__burger--active");
+    nav.classList.remove("main-header__nav--active");
+})
+
 
 window.onresize = viewport;
 
